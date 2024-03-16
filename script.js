7b48b097a3f363baf53c2eb2d825ccdfdf57d3ae
Juliyamonrea <134256875+Juliyamonrea@users.noreply.github.com>
2024-03-16 12:03:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+var student ={
+	name :"Kiran"
+	
+};
+
+Object.prototype.getKeys = function(){
+	return Object.keys(this);
+};
+
+console.log(student.getKeys());
